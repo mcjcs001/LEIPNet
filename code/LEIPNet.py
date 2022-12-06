6b9a76979281bff4330262bb9fac37535adc89bb
mcjcs001 <115930641+mcjcs001@users.noreply.github.com>
2022-12-06 12:56:57 +0800

    part_of_the_project_file

---
+++ code/LEIPNet.py
@@ -0,0 +1,136 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from nets.xception import xception
+from nets.mobilenetv2 import mobilenetv2
+
+
+class simam_module(torch.nn.Module):
+    def __init__(self, channels=None, e_lambda=1e-4):
+        super(simam_module, self).__init__()
+
+        self.activaton = nn.Sigmoid()
+        self.e_lambda = e_lambda
+
+    def __repr__(self):
+        s = self.__class__.__name__ + '('
+        s += ('lambda=%f)' % self.e_lambda)
+        return s
+
+    @staticmethod
+    def get_module_name():
+        return "simam"
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        n = w * h - 1
+
+        x_minus_mu_square = (x - x.mean(dim=[2, 3], keepdim=True)).pow(2)
+        y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2, 3], keepdim=True) / n + self.e_lambda)) + 0.5
+
+        return x * self.activaton(y)
+
+
+
+class Output(nn.Module):
+    def __init__(self, in_chan, mid_chan, n_classes, up_factor=32, *args, **kwargs):
+        super(Output, self).__init__()
+        self.up_factor = up_factor
+        out_chan = n_classes
+        self.conv = ConvBNReLU(in_chan, mid_chan, ks=3, stride=1, padding=1)
+        self.conv_out = nn.Conv2d(mid_chan, out_chan, kernel_size=1, bias=True)
+        self.up = nn.Upsample(scale_factor=up_factor,
+                mode='bilinear', align_corners=False)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.conv_out(x)
+        x = self.up(x)
+        return x
+
+
+class LEIPNet(nn.Module):
+    def __init__(self, num_classes, backbone="mobilenet", pretrained=True, downsample_factor=16):
+        super(LEIPNet, self).__init__()
+        if backbone=="xception":
+            #----------------------------------#
+            #   获得两个特征层
+            #   浅层特征    [128,128,256]
+            #   主干部分    [30,30,2048]
+            #----------------------------------#
+            self.backbone = xception(downsample_factor=downsample_factor, pretrained=pretrained)
+            in_channels = 2048
+            low_level_channels = 256
+        elif backbone=="mobilenet":
+            #----------------------------------#
+            #   获得两个特征层
+            #   浅层特征    [128,128,24]
+            #   主干部分    [30,30,320]
+            #----------------------------------#
+            self.backbone = MobileNetV2(downsample_factor=downsample_factor, pretrained=pretrained)
+            in_channels = 320
+            low_level_channels = 24
+        else:
+            raise ValueError('Unsupported backbone - `{}`, Use mobilenet, xception.'.format(backbone))
+
+        #-----------------------------------------#
+        #   ASPP特征提取模块
+        #   利用不同膨胀率的膨胀卷积进行特征提取
+        #-----------------------------------------#
+        self.stage1 = RSU4F(320, 32, 256)
+        
+        #----------------------------------#
+        #   浅层特征边
+        #----------------------------------#
+        self.shortcut_conv = nn.Sequential(
+            nn.Conv2d(low_level_channels, 48, 1),
+            nn.BatchNorm2d(48),
+            nn.ReLU(inplace=True)
+        )		
+
+        self.cat_conv = nn.Sequential(
+            nn.Conv2d(64+256, 256, 3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
+            nn.Conv2d(256, 256, 3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(0.1),
+        )
+        self.cls_conv = nn.Conv2d(256, num_classes, 1, stride=1)
+        self.stem = Spatial_down(3, 64, k=3)
+        self.S2 = nn.Sequential(
+            ConvBNReLU(64, 64, 3, stride=2),
+            ConvBNReLU(64, 64, 3, stride=1),
+            ConvBNReLU(64, 64, 3, stride=1),
+        )
+        self.positioning = simam_module()
+        self.ffm = FeatureFusionModule(320, 256)
+        self.oup = Output(256, 64, 2, up_factor=4)
+
+    def forward(self, x):
+        H, W = x.size(2), x.size(3)
+        #-----------------------------------------#
+        #   获得两个特征层
+        #   low_level_features: 浅层特征-进行卷积处理
+        #   x : 主干部分-利用ASPP结构进行加强特征提取
+        #-----------------------------------------#
+        low_level_features = self.stem(x)
+        low_level_features = self.S2(low_level_features)
+        low_level_features = self.positioning(low_level_features)
+        x = self.backbone(x)
+        x = self.stage1(x)
+        x = self.positioning(x)
+        #-----------------------------------------#
+        #   将加强特征边上采样
+        #   与浅层特征堆叠后利用卷积进行特征提取
+        #-----------------------------------------#
+        x = F.interpolate(x, size=(low_level_features.size(2), low_level_features.size(3)), mode='bilinear', align_corners=True)
+        P00 = self.ffm(low_level_features, x)
+        x = self.oup(P00)
+        return x
+
